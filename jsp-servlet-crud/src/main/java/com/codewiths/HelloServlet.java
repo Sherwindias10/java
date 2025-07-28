059ed8539ddf2b8b2147d51d5fc8dd4a07ba2639
Sherwindias10 <sherwindias56@gmail.com>
2025-07-28 12:09:53 +0530

    Add files via upload

---
+++ jsp-servlet-crud/src/main/java/com/codewiths/HelloServlet.java
@@ -0,0 +1,37 @@
+package com.codewiths;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello-servlet")
+public class HelloServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+    public HelloServlet(){
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.setContentType("/text/html");
+
+        PrintWriter out = resp.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Servlet HelloServlet</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Servlet HelloServlet</h1>");
+        out.println("</body>");
+        out.println("</html>");
+
+        out.close();
+    }
+}
